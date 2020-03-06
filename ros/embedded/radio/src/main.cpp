35315a55add53b8776ba6f3cfa120db850f16308
uasif13 <uasif13@gmail.com>
2020-03-06 16:20:39 -0500

    initial commit for radio branch

---
+++ ros/embedded/radio/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+// https://github.com/thotro/arduino-dw1000
+// https://github.com/derindutz/jarvas-indoor-positioning-system
+// https://www.decawave.com/sites/default/files/resources/DWM1000-Datasheet-V1.6.pdf
+// talk to the board
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
